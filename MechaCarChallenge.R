c0e19abc2aeb3647c515ce44b45549306ebb97fb
Valeriia161 <leraozdoeva@gmail.com>
2022-11-19 13:09:32 -0500

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -16,3 +16,17 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 # p-value: 5.35e-11
 
 # Deliverable 2
+
+# Read csv
+del2_table <- read.csv(file = 'Suspension_Coil.csv',check.names=F,stringsAsFactors = F )
+head(del2_table)
+ 
+# Create total summary
+summaty_total <- del2_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
+
+# Create lot summary
+summary_lot <- del2_table %>% group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
+
+# Deliverable 3
